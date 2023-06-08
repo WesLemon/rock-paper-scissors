bd9aadef459061e5913090ef39d635cfa02de1d1
wes-ubuntu-desktop <wesleyshaw25@gmail.com>
2023-06-08 11:14:42 +0100

    Added computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+function getComputerChoice() {
+    let choiceArray = [
+        'Rock',
+        'Paper',
+        'Scissors'
+    ]
+    let choice = Math.floor(Math.random()*3)
+    return choiceArray[choice]
+}
+
+console.log(getComputerChoice())
