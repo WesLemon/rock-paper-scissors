db4feffa66a33d0fcaeab0c2156aec3372a78fab
wes-ubuntu-desktop <wesleyshaw25@gmail.com>
2023-06-08 11:24:57 +0100

    Added single round functionaliy with hard-coded 'Rock'

--- script.js
+++ script.js
@@ -8,4 +8,25 @@ function getComputerChoice() {
     return choiceArray[choice]
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    player = playerSelection.toLowerCase()
+    computer = computerSelection.toLowerCase()
+
+    if((player == 'rock' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'paper') ||
+        (player == 'paper' && computer == 'rock')) {
+            return "You win!"
+        }
+
+    else if(player == computer) {
+        return "Draw!"
+    }
+
+    else {
+        return "You lose!"
+    }
+}
+
+computerChoice = getComputerChoice()
+console.log("Computer: " + computerChoice)
+console.log(playRound('Rock', computerChoice))
